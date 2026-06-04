b9226bd0abb83097874aafb10c4a2a59a6651599
janbryledionisio-stratpoint <janbryle.dionisio@stratpoint.com>
2026-06-04 21:03:47 +0800

    Filtering test cases for general standards

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from fetch_cases import (
     fetch_test_cases
 )
 
+from general_standards_filter import (
+    save_all_cases
+)
+
 def main():
     endpoint_file = generate_endpoint_file(
         "data/GRCX-web.csv"
@@ -15,12 +19,15 @@ def main():
         endpoint_file
     )
 
-    for result in results:
-        print("ID:", result["ID"])
-        print("Status:", result["status_code"])
-        print("Response:", result["response"])
-        print("-" * 50)
+    # for result in results:
+    #     print("ID:", result["ID"])
+    #     print("Status:", result["status_code"])
+    #     print("Response:", result["response"])
+    #     print("-" * 50)
 
+    save_all_cases(
+        results
+    )
 
 if __name__ == "__main__":
     main()
